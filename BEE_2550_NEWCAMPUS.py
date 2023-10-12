30bb66dc9a5683f1ff0290f700b5667dfd2c218c
Sebastian Zuzunaga <seb55381@hotmail.com>
2023-10-12 00:47:11 -0500

    Modified implementation (needs verification)

--- BEE_2550_NEWCAMPUS.py
+++ BEE_2550_NEWCAMPUS.py
@@ -2,18 +2,21 @@ import heapq
 
 mst = []
 usedVertices = set()
-with open("sasassas") as f:
-    numVertices = int(f.readline())
-    edges = [[] for _ in range(numVertices)]
-    for line in f.readlines():
-        edge = tuple(map(int, line.split("")))
-        if edge[0] == edge[1]: continue
-        heapq.heappush(edges[edge[0]],(edge[2], edge[1]))
-        heapq.heappush(edges[edge[1]],(edge[2], edge[0]))
+
+n_buildings = int(input())
+n_paths = int(input())
+
+edges = [[] for _ in range(n_buildings)]
+for _ in range(n_paths):
+    path = input()
+    edge = tuple(map(int, path.split()))
+    if edge[0] == edge[1]: continue
+    heapq.heappush(edges[edge[0]-1], (edge[2], edge[1]-1))
+    heapq.heappush(edges[edge[1]-1], (edge[2], edge[0]-1))
 
 cost, dest = 0, 1
-while len(usedVertices) < numVertices:
-    vertexWithSmallestEdge = 0
+while len(usedVertices) < n_buildings:
+    vertexWithSmallestEdge = 1
     for vertex in usedVertices:
         while len(edges[vertex]) > 0 and edges[vertex][0][dest] in usedVertices:
             heapq.heappop(edges[vertex])
@@ -24,8 +27,8 @@ while len(usedVertices) < numVertices:
             vertexWithSmallestEdge = vertex
 
     edge = heapq.heappop(edges[vertexWithSmallestEdge])
-    mst.append((vertexWithSmallestEdge, edge[dest], edge[cost]))
+    mst.append((vertexWithSmallestEdge+1, edge[dest]+1, edge[cost]))  # Adding 1 to vertex numbers
     usedVertices.add(vertexWithSmallestEdge)
     usedVertices.add(edge[dest])
 
-    print(mst)
+print(mst)
