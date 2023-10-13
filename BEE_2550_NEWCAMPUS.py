caaafc3372ee3b27e6c39e7c497f3c5142cfa906
Sebastian Zuzunaga <seb55381@hotmail.com>
2023-10-13 12:05:00 -0500

    changed implementation (last was too long)

--- BEE_2550_NEWCAMPUS.py
+++ BEE_2550_NEWCAMPUS.py
@@ -1,4 +1,4 @@
-import heapq
+import queue
 
 while True:
     mst = []
@@ -8,29 +8,35 @@ while True:
         n_buildings, n_paths = map(int, input().split())
     except EOFError:
         break
-    
+
     edges = [[] for _ in range(n_buildings)]
+    min_edges = [float('inf')] * n_buildings 
+
     for _ in range(n_paths):
         edge = tuple(map(int, input().split()))
         if edge[0] == edge[1]: continue
-        heapq.heappush(edges[edge[0]-1], (edge[2], edge[1]-1))
-        heapq.heappush(edges[edge[1]-1], (edge[2], edge[0]-1))
+        edges[edge[0]-1].append((edge[2], edge[1]-1))
+        edges[edge[1]-1].append((edge[2], edge[0]-1))
 
-    cost, dest = 0, 1
-    while len(usedVertices) < n_buildings: #change required here
-        vertexWithSmallestEdge = 1
-        for vertex in usedVertices:
-            while len(edges[vertex]) > 0 and edges[vertex][0][dest] in usedVertices:
-                heapq.heappop(edges[vertex])
+        if edge[2] < min_edges[edge[0]-1]:
+            min_edges[edge[0]-1] = edge[2]
+        if edge[2] < min_edges[edge[1]-1]:
+            min_edges[edge[1]-1] = edge[2]
 
-            if len(edges[vertex]) == 0: continue
+    pq = queue.PriorityQueue()
+    for i in range(1, n_buildings):
+        pq.put((min_edges[i], i))
 
-            if len(edges[vertexWithSmallestEdge]) == 0 or edges[vertex][0][cost] < edges[vertexWithSmallestEdge][0][cost]:
-                vertexWithSmallestEdge = vertex
+    total_cost = 0
+    while not pq.empty():
+        cost, vertex = pq.get()
+        if vertex in usedVertices:
+            continue
+        usedVertices.add(vertex)
+        total_cost += cost
 
-        edge = heapq.heappop(edges[vertexWithSmallestEdge])
-        mst.append((vertexWithSmallestEdge+1, edge[dest]+1, edge[cost]))  # Adding 1 to vertex numbers
-        usedVertices.add(vertexWithSmallestEdge)
-        usedVertices.add(edge[dest])
-    total_cost = sum(edge[2] for edge in mst)
+        for edge_cost, neighbor in edges[vertex]:
+            if neighbor not in usedVertices:
+                pq.put((edge_cost, neighbor))
+
     print(total_cost)
